2086171a018578be7a7150ee80e785fb5f4f86b2
Hello World <39833662+hlwrld@users.noreply.github.com>
2018-11-06 14:35:29 +0100

    Made it possible to configure the chart

--- src/server.js
+++ src/server.js
@@ -3,9 +3,39 @@ const _ = require('lodash');
 const moment = require('moment');
 const redis = require('redis');
 const express = require('express');
+const request = require('request-json');
 
 const env = process.env;
+
 const port = env.PORT;
+const access_key = env.ACCESS_KEY;
+const apiURL = env.API_URL;
+const locationsURL = env.LOCATIONS_URL;
+const locationFilter = new RegExp(env.LOCATION_FILTER);
+
+const chartLocations= (env.CHART_LOCATIONS || '').split(',').filter(location => location.length > 0);
+const chartPriceMin = parseFloat(env.CHART_PRICE_MIN);
+const chartPriceMax = parseFloat(env.CHART_PRICE_MAX);
+const chartDays = parseInt(env.CHART_DAYS);
+
+const lastmodified = '1970-01-01';
+
+console.log('ACCESS_KEY=', access_key);
+console.log('LOCATIONS_URL=',locationsURL);
+console.log('LOCATION_FILTER=', locationFilter);
+
+console.log('CHART_LOCATIONS=', chartLocations);
+console.log('CHART_PRICE_MIN=', chartPriceMin);
+console.log('CHART_PRICE_MAX=', chartPriceMin);
+console.log('CHART_DAYS=', chartPriceMax);
+
+const httpClient = request.createClient(apiURL);
+
+async function getLocations() {
+  const response = await httpClient.post(locationsURL, {access_key, lastmodified});
+  return response.body
+    .filter(location => locationFilter.test(location.address));
+}
 
 async function getPrices() {
   if (!env.REDIS_URL) {
@@ -25,26 +55,43 @@ async function getPrices() {
 
 async function chartDataSets() {
   const prices = await getPrices();
+  const locations = await getLocations();
   return _(prices)
     .map((priceString, key) => {
-      const id = parseInt(key.substring(25));
+      const id = key.substring(25);
       const date = moment(key.substring(0, 24));
       const price = parseFloat(priceString);
       return {id, date, price};
     })
-    .filter(value => value.price > 10 && value.price < 20 && value.date.diff(moment(), 'days') > -30)
+    .filter(
+      value => (chartLocations.length === 0 || chartLocations.includes(value.id))
+        && value.price > chartPriceMin
+        && value.price < chartPriceMax
+        && moment().diff(value.date, 'days') < chartDays
+    )
     .sortBy('date')
     .groupBy('id')
-    .mapValues((values, key) => ({
-      label: key,
-      data: values.map(value => ({x: value.date, y: value.price})),
-      fill: false
-    }))
+    .mapValues((values, id) =>
+      ({
+        label: _.find(locations, {id: parseInt(id)}).name,
+        data: values.map(value => ({x: value.date, y: value.price})),
+        fill: false
+      }))
     .values()
     .value();
 }
 
 const app = express();
+
+app.get('/locations', (request, response) => {
+  getLocations()
+    .then(locations => ({locations}))
+    .then(response.send.bind(response))
+    .catch(error => {
+      console.error(error);
+      return response.sendStatus(500);
+    });
+});
 
 app.get('/chart', (request, response) => {
   chartDataSets()
